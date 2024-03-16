604d304f29f4763ed9ffcd5e448b920bd4770a96
zohiros <zohir96os@gmail.com>
2024-03-16 14:38:46 +0100

    Test api created

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
+//import packages
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+//import routers
+import userRoute from "./routes/user.route.js";
 
+//Config env
 dotenv.config();
 
+//Call env vars
 const PORT = process.env.PORT || 3000;
 const URI = process.env.URI;
 
+//Configure mongoose
 mongoose
   .connect(URI)
   .then(() => {
@@ -16,7 +22,10 @@ mongoose
     console.log(err);
   });
 
+//Express
 const app = express();
+
+app.use("/api/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is up & runing on port ${PORT}`);
