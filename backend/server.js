994c98638a887ffc8b02d2f6156615dba9b175d7
zohiros <zohir96os@gmail.com>
2024-03-16 15:36:28 +0100

    Create midleware  for error handling

--- backend/server.js
+++ backend/server.js
@@ -35,3 +35,13 @@ app.use("/api/auth", authRoute);
 app.listen(PORT, () => {
   console.log(`Server is up & runing on port ${PORT}`);
 });
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
