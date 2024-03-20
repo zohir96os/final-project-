0277aa9b63057cc2a68836208ce1c2cc3a817083
zohiros <zohir96os@gmail.com>
2024-03-20 23:24:24 +0100

    Created private route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from "./pages/Dashboard";
 import Blogs from "./pages/Blogs";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 const App = () => {
   return (
     <div>
@@ -17,7 +18,10 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-up" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+
           <Route path="/blogs" element={<Blogs />} />
         </Routes>
         <Footer />
