e5434a1df574a5ed88d96d10a08e285c8fef4734
zohiros <zohir96os@gmail.com>
2024-03-24 21:51:54 +0100

    Fix delete user function

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -51,7 +51,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
     return next(errorHandler(400, "Action not allowed"));
   }
   try {
