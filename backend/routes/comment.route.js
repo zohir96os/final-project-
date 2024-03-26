b19ae503063ae29e31017f809e404681e4f442cc
zohiros <zohir96os@gmail.com>
2024-03-26 01:09:00 +0100

    Added like function

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -2,10 +2,12 @@ import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
 import { creatComment } from "../controllers/comment.controller.js";
 import { getPostComment } from "../controllers/comment.controller.js";
+import { likeComment } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, creatComment);
 router.get("/getPostComments/:postId", getPostComment);
+router.put("/likeComment/:commentId", verifyToken, likeComment);
 
 export default router;
