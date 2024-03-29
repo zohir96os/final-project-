8a2680b5b4b58b000785044dfad7585bacdcfd4a
zohiros <zohir96os@gmail.com>
2024-03-30 00:27:44 +0100

    Complete about and blogs pages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,22 @@
-import React from "react";
-
 const About = () => {
-  return <div>about</div>;
+  return (
+    <div className="min-h-screen flex itemes-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center ">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Blogos
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p className="">
+              Welcome to my blog, where stories meet exploration. Join me on a
+              journey through a myriad of topics and experiences. Let`s dive
+              into curiosity together and uncover the wonders that await.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import CallToAction from "../components/CallToAction";
 
 const Blogs = () => {
-  return <div>blogs</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Blogs</h1>
+      <p className="text-md text-gray-500 ">
+        Blogs are online platforms where people share their thoughts, expertise,
+        and experiences on various topics, fostering community and discussion
+        around shared interests.
+      </p>
+      <CallToAction />
+    </div>
+  );
 };
 
 export default Blogs;
