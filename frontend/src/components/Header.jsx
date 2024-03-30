c59f6b8fbc0217909045c74eba9e60627fba607d
zohiros <zohir96os@gmail.com>
2024-03-30 01:02:24 +0100

    Search button fixed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -86,12 +86,17 @@ const Header = () => {
             onChange={(e) => setSearchTerm(e.target.value)}
           />
         </form>
-        <Button className="w-12 h-10  lg:hidden" color="gray" pill>
+        <Button
+          className="w-12 h-10  lg:hidden "
+          color="gray"
+          pill
+          href="/search"
+        >
           <AiOutlineSearch />
         </Button>
         <div className="flex gap-2 md:order-2">
           <Button
-            className="w-12 h-10 hidden sm:inline"
+            className="w-12 h-10  sm:inline"
             color="gray"
             pill
             onClick={() => dispatch(toggleTheme())}
